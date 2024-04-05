3f996987e9b3cc103d9dbd71906d133373015179
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-04-05 21:09:01 +0300

    auth_tests_cypress commit

--- cypress/e2e/form-tests/auth-form.cy.js
+++ cypress/e2e/form-tests/auth-form.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 describe('example to-do app', () => {
-    it("authorization invalid_creds 1", () => {
+    it("authorization invalid_password", () => {
         cy.visit('https://the-internet.herokuapp.com/login');
         cy.get('input#username').type('tomsmith');
         cy.get('input#password').type('Password!!');
         cy.get('button[type="submit"').click();
         cy.get('div#flash').should('have.text', '\n Your password is invalid!\n ×\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
     })
-    it("authorization invalid_creds 2H", () => {
+    it("authorization invalid_username", () => {
         cy.visit('https://the-internet.herokuapp.com/login');
         cy.get('input#username').type('galya');
         cy.get('input#password').type('SuperSecretPassword!');
