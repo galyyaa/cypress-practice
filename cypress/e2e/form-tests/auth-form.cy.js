3574d9f3c210858f121dd681d92c534c83acc8f1
Galya Morgun <galya_morgun@MacBook-Air-Galya.local>
2024-04-05 20:14:27 +0300

    auth_tests_cypress commit

--- cypress/e2e/form-tests/auth-form.cy.js
+++ cypress/e2e/form-tests/auth-form.cy.js
@@ -1,17 +1,18 @@
 /// <reference types="cypress" />
 describe('example to-do app', () => {
-    it("authorization test 1", () => {
+    it("authorization invalid_creds 1", () => {
         cy.visit('https://the-internet.herokuapp.com/login');
         cy.get('input#username').type('tomsmith');
         cy.get('input#password').type('Password!!');
         cy.get('button[type="submit"').click();
-        cy.get('h2').should('have.text', ' Secure Area');
+        cy.get('div#flash').should('have.text', '\n Your password is invalid!\n ×\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
     })
-    it("authorization test 2", () => {
+    it("authorization invalid_creds", () => {
         cy.visit('https://the-internet.herokuapp.com/login');
         cy.get('input#username').type('galya');
         cy.get('input#password').type('SuperSecretPassword!');
         cy.get('button[type="submit"').click();
+        cy.get('div#flash').should('have.text', '\n Your password is invalid!\n ×\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
         cy.url().should('eq', 'https://the-internet.herokuapp.com/secure');
     })
 })
